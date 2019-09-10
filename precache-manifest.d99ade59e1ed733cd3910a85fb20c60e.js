9161d79c93e3da6f2921699443aee9512a38e170
Christian Páez <frontend2@haytic.com>
2019-09-09 21:26:46 -0500

    Updates

---
+++ precache-manifest.d99ade59e1ed733cd3910a85fb20c60e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a754238471f11fa834f196d30055724d",
+    "url": "/quote-generator-app/index.html"
+  },
+  {
+    "revision": "dfb096678a952017d574",
+    "url": "/quote-generator-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1096875da24b5867468f",
+    "url": "/quote-generator-app/static/js/2.13e52cd1.chunk.js"
+  },
+  {
+    "revision": "dfb096678a952017d574",
+    "url": "/quote-generator-app/static/js/main.43b12b0e.chunk.js"
+  },
+  {
+    "revision": "0df786f52adf5a611c35",
+    "url": "/quote-generator-app/static/js/runtime~main.26fabff2.js"
+  }
+]);
